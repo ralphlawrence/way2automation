e817e8a9783447980f7514bbb47e2a62c84737fa
ralphlawrence <ralphlawrence1916@gmail.com>
2025-08-26 23:47:44 +0800

    --  setup the cypress package. added initial test scripts for navigating the webpage and listing the action name categories

---
+++ cypress/integration/1_navigate.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Way2Automation Demo Site', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    Cypress.on('uncaught:exception', (err) => {
+      if (err.message.includes('jQuery is not defined')) {
+        return false
+      }
+    })
+  });
+
+  it('Navigate to the web page', () => {
+    cy.contains('Test your Selenium / QTP Scripts').should('be.visible')
+    
+  });
+
+
+});
---
+++ cypress/integration/2_list_actionName.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('List Action Name category', () => {
+    beforeEach(() => {
+        cy.visit('/')
+        Cypress.on('uncaught:exception', (err) => {
+            if (err.message.includes('jQuery is not defined')) {
+                return false
+            }
+        })
+        cy.fixture('actionNames').as('actionNames')
+    });
+
+    it('should validate presence of each action button', function () {
+        Object.entries(this.actionNames).forEach(([category, actions]) => {
+            cy.log(`Checking category: ${category}`)
+            actions.forEach((action) => {
+                cy.contains(action, { matchCase: false })
+                    .scrollIntoView()
+                    .should('be.visible')
+            })
+        })
+    })
+})
