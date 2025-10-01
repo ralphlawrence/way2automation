af4514afb5d0798d0d4f34b78d9a6ba28de2a82e
ralphlawrence <ralphlawrence1916@gmail.com>
2025-10-01 22:16:13 +0800

    Added a project ID in config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'uk7zre',
   viewportWidth: 1920,
   viewportHeight: 1080,
   defaultCommandTimeout: 8000,
